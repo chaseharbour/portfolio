bb3864d72b64448fad32fa8e7353d508e67eb748
chaseharbour <harbourcp@gmail.com>
2021-07-06 19:26:19 -0700

    prog(About, Resume): Finished current iteration of about styles, started updating contact styles.

--- components/Resume.js
+++ components/Resume.js
@@ -3,21 +3,24 @@ import resumeStyles from "../styles/_resume.module.scss";
 
 const Resume = () => {
   return (
-    <article className={resumeStyles.container}>
-      <h2 className={resumeStyles.header}>CV.</h2>
-      <p className={resumeStyles.body}>
-        My CV is located at the link below. If you'd like to reach me, email is
-        always a great option: harbourcp@gmail.com
-      </p>
-      <a
-        className={resumeStyles.link}
-        href="https://read.cv/chaseharbour"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Visit CV
-      </a>
-    </article>
+    <>
+      <h1 className={resumeStyles.title}>Contact</h1>
+      <article className={resumeStyles.container}>
+        <h2 className={resumeStyles.header}>Cv</h2>
+        <p className={resumeStyles.body}>
+          My CV is located at the link below. If you'd like to reach me, email
+          is always a great option: harbourcp@gmail.com
+        </p>
+        <a
+          className={resumeStyles.link}
+          href="https://read.cv/chaseharbour"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit CV
+        </a>
+      </article>
+    </>
   );
 };
 
