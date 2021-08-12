7d587ddaf524dece486cf55c8c835d18f9348a09
chaseharbour <harbourcp@gmail.com>
2021-08-12 15:33:14 -0700

    feat(): initial commit

--- components/About.js
+++ components/About.js
@@ -8,15 +8,16 @@ const About = () => {
       <article className={aboutStyles.container}>
         <h2 className={aboutStyles.aboutHeader}>Where I'm Going.</h2>
         <p className={aboutStyles.aboutBodyPrimary}>
-          Hello, my name is Chase! I am a junior developer looking to expand my
-          skillset to help individuals and companies create and interact with
-          meaningful web experiences.
+          Hello, my name is Chase! I am a junior developer looking to join a
+          team where I can expand my skillset and hone my craft. My passion is
+          to help individuals and companies create and interact with meaningful
+          web experiences.
         </p>
         <h2 className={aboutStyles.aboutHeader}>Where I've Been.</h2>
         <p className={aboutStyles.aboutBodySecondary}>
-          My interest in web development stems from my time as a teacher, where
+          My interest in web development stems from my time as a teacher where
           web technologies brought content to a diverse group of individuals in
-          a meaningful and accessible way. A passion was sparked and I sought to
+          meaningful and accessible ways. A passion was sparked and I sought to
           build my skills to bring those quality experiences to as many others
           as possible.
         </p>
