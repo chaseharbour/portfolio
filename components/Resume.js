720e4b702486179ad0b70bd20d8891a89ead6f6a
chaseharbour <harbourcp@gmail.com>
2021-08-16 18:57:12 -0700

    prog(resume): Mobile styling complete, contact form added and styled.

--- components/Resume.js
+++ components/Resume.js
@@ -8,18 +8,51 @@ const Resume = () => {
     <>
       <h1 className={resumeStyles.title}>Contact</h1>
       <article className={resumeStyles.container}>
+        <h2 className={resumeStyles.subHead}>Let's get in touch.</h2>
         <p className={resumeStyles.body}>
-          My experience and work history is located at the link below. If you'd
-          like to reach me, email is always a great option: harbourcp@gmail.com
+          I would love to hear from you! If you'd like to know more you can view
+          my work history{" "}
+          <a
+            className={resumeStyles.animLink}
+            href="https://read.cv/chaseharbour"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          . Otherwise, feel free to shoot me a message!
         </p>
-        <a
-          className={resumeStyles.link}
-          href="https://read.cv/chaseharbour"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit CV
-        </a>
+        <form className={resumeStyles.form}>
+          <label for="name">Name</label>
+          <input
+            className={resumeStyles.formName}
+            type="text"
+            name="name"
+            placeholder="Name *"
+            required
+          />
+          <label for="email">Email</label>
+          <input
+            className={resumeStyles.formEmail}
+            type="email"
+            name="email"
+            placeholder="Email *"
+            required
+          />
+          <label for="subject">Subject</label>
+          <input
+            className={resumeStyles.formSubject}
+            type="text"
+            name="subject"
+            placeholder="Subject"
+          />
+          <label for="message">Message</label>
+          <textarea
+            className={resumeStyles.formMessage}
+            name="message"
+            placeholder="Message..."
+          />
+        </form>
       </article>
     </>
   );
